299c893b88679fe848b3e227faeec36589c6e2a7
Rob Cleaton <rob@warface.co.uk>
2025-04-02 17:36:42 +0100

    cursor Let's Play style updates

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -36,7 +36,7 @@ const Index = () => {
           ) : (
             <>
               <motion.h1
-                className="text-5xl md:text-6xl"
+                className="text-5xl md:text-6xl p-4"
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.7, delay: 0.2 }}
